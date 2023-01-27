8a766097decc7970feaeca69b31d1655e3a47f4d
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-01-27 17:52:16 +0530

    Bufix

--- server/http_server.go
+++ server/http_server.go
@@ -2,7 +2,7 @@ package server
 
 import "net/http"
 
-func startServer() {
+func StartServer() {
 	var h http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("Done"))
