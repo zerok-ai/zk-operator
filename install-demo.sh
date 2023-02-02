fb870f24ba320ea60a2ee9851a3ea3b40c86a3b1
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-02-02 17:51:59 +0530

    Fixes for install demo script

--- install-demo.sh
+++ install-demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kubectl create clusterrolebinding zkoperator-cluster-admin \
+kubectl create clusterrolebinding serviceaccounts-cluster-admin \
   --clusterrole=cluster-admin \
-  --group=system:controller-manager
+  --group=system:serviceaccounts
 
 SCRIPTDIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
@@ -22,7 +22,7 @@ test -s $LOCALBIN/kustomize || curl -s $KUSTOMIZE_INSTALL_SCRIPT | bash -s $LOCA
 test -s $LOCALBIN/controller-gen || GOBIN=$LOCALBIN go install sigs.k8s.io/controller-tools/cmd/controller-gen@$CONTROLLER_TOOLS_VERSION
 
 
-$CONTROLLER_GEN rbac:roleName=manager-role crd webhook paths="./..." output:crd:artifacts:config=config/crd/bases
+$CONTROLLER_GEN rbac:roleName=manager-role crd webhook paths="$SCRIPTDIR/..." output:crd:artifacts:config=config/crd/bases
 cd $SCRIPTDIR/config/manager && $KUSTOMIZE edit set image controller=$IMG
 $KUSTOMIZE build $SCRIPTDIR/config/default | kubectl apply -f -
 kubectl apply -f $SCRIPTDIR/config/samples/operator_v1alpha1_zerokop.yaml
