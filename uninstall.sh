4e5b11264cbf2d3ffe66e3d6b2175b255022595b
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-04-11 14:40:35 +0530

    Removing redundant deletion of zerokops resource.

--- uninstall.sh
+++ uninstall.sh
@@ -5,4 +5,3 @@ scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 kubectl delete mutatingwebhookconfiguration zerok-webhook
 kubectl delete namespace zerok-injector
 make -C ${scriptDir} undeploy
-kubectl delete -f $scriptDir/config/samples/operator_v1alpha1_zerokop.yaml
