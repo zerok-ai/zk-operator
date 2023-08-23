fdbe88340a6f02ef6420aee181ac33ed2c8c9dd3
avingoyal <avingoyal01+github@gmail.com>
2023-08-23 17:16:07 +0530

    minor fixes

--- helm-charts/rename-docker-repo.sh
+++ helm-charts/rename-docker-repo.sh
@@ -7,5 +7,5 @@ echo "ZK_INIT_TAG=$ZK_INIT_TAG"
 
 # Set the image tag based on the chart version
 perl -pi -e "s#dockerBase: \".*\"#dockerBase: \"$DOCKER_REPO\"#" $THIS_DIR/values.yaml
-perl -pi -e "s#zkInitContainerRepo: \".*\"#dockerBase: \"$ZK_INIT_REPO\"#" $THIS_DIR/values.yaml
-perl -pi -e "s#zkInitContainerTag: \".*\"#dockerBase: \"$ZK_INIT_TAG\"#" $THIS_DIR/values.yaml
+perl -pi -e "s#zkInitContainerRepo: \".*\"#zkInitContainerRepo: \"$ZK_INIT_REPO\"#" $THIS_DIR/values.yaml
+perl -pi -e "s#zkInitContainerTag: \".*\"#zkInitContainerTag: \"$ZK_INIT_TAG\"#" $THIS_DIR/values.yaml
