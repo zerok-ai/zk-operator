f17fc2d8a0f6ed6d235496be7f0fe3feda4c5748
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-10-14 17:40:47 +0530

    updated script

--- app-start.sh
+++ app-start.sh
@@ -19,13 +19,13 @@ done
 
 arch=$(uname -m)
 
-ls
-
 if [ "$arch" = "x86_64" ] || [ "$arch" = "amd64" ]; then
     echo "This is an AMD64 system."
+    echo "Command is $amd64"
     ./$amd64
 elif [ "$arch" = "aarch64" ] || [ "$arch" = "arm64" ]; then
     echo "This is an ARM64 system."
+    echo "Command is $arm64"
     ./$arm64
 else
     echo "Unsupported Architecture: $arch"
