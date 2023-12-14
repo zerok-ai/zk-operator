366f97338c6ad6c61b494a7ecb4dee486f6fd66b
CHNG7 <nikhilchinta7@gmail.com>
2023-12-14 12:30:43 +0530

    add k8s deepcopy comment

--- api/v1alpha1/zerokprobe_types.go
+++ api/v1alpha1/zerokprobe_types.go
@@ -1,39 +1,32 @@
 package v1alpha1
 
 import (
+	"github.com/zerok-ai/zk-utils-go/scenario/model"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-//+k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen=true
+type Workloads map[string]model.Workload
 
-type Workloads map[string]Workload
-
+// +k8s:deepcopy-gen=true
 type ZerokProbeSpec struct {
-	Title     string      `json:"title"`
-	Enabled   bool        `json:"enabled"`
-	Workloads Workloads   `json:"workloads"`
-	Filter    Filter      `json:"filter,omitempty"`
-	GroupBy   []GroupBy   `json:"group_by,omitempty"`
-	RateLimit []RateLimit `json:"rate_limit,omitempty"`
+	Title     string            `json:"title"`
+	Enabled   bool              `json:"enabled"`
+	Workloads Workloads         `json:"workloads,omitempty"`
+	Filter    model.Filter      `json:"filter,omitempty"`
+	GroupBy   []model.GroupBy   `json:"group_by,omitempty"`
+	RateLimit []model.RateLimit `json:"rate_limit,omitempty"`
 }
 
-type ZerokProbeStatus struct {
-	IsCreated bool `json:"is_created,omitempty"`
-}
-
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // above line acts a code marker recognized by Kube builder and root=true mean this is the root object
 // ZerokProbe is the CRD schema for crating probe
 type ZerokProbe struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   ZerokProbeSpec   `json:"spec,omitempty"`
-	Status ZerokProbeStatus `json:"status,omitempty"`
-}
-
-func init() {
-	SchemeBuilder.Register(&ZerokProbe{})
+	Spec              ZerokProbeSpec `json:"spec,omitempty"`
 }
